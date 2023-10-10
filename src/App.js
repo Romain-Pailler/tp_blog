416d7ac75f9014a83f2b56ad6a5741f8023d487c
Romain-Pailler <romain.pailler77@gmail.com>
2023-10-10 14:16:36 +0200

    add Routes from router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Banniere from './component/Banniere';
 import Footer from './component/Footer';
 import UserCard from './component/UserCard';
-
 function App() {
   return(
     <div>
-    <Banniere/>
-    <UserCard/>
-    <Footer/>
+      <Banniere/>
+      <Routes>
+        <Route path="/" element={<UserCard/>}/> 
+      </Routes>
+      <Footer/>
     </div>
   );
 }
