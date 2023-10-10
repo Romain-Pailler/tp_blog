98864ec68422769489623bac0b05b2550ba2c7bc
Romain-Pailler <romain.pailler77@gmail.com>
2023-10-10 09:43:55 +0200

    Ajoute le composant Banniere avec son css

---
+++ src/component/Banniere.jsx
@@ -0,0 +1,9 @@
+import '../style/Banniere.css';
+function Banniere(){
+    return (
+    <div className="banniere">
+        <h1>Bienvenue sur Blogeo</h1>
+    </div>
+    );
+}
+export default Banniere;
